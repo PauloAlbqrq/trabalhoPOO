d578686e6fa91bd08ff1a8df9e20989115f3d46e
Matheus <matheushenriquesl06@gmail.com>
2025-02-10 01:06:46 -0400

    adicionei checagem da existencia do usuario, do livro, mudei os loops para usar uma variavel bool ao inves de true, consertei um problema que dava com o nextLine pegando a quebra de linha, e criação de emprestimo, se tiver funcionando mesmo.

--- Main.java
+++ Main.java
@@ -7,74 +7,121 @@ public class Main {
   public static ArrayList<livro> livros = new ArrayList<livro>();
   public static ArrayList<emprestimo> emprestimos = new ArrayList<emprestimo>();
 
-  public static void menuRegistro(){
-    Scanner subMenuRegistro = new Scanner(System.in);
+  public static Scanner scanner = new Scanner(System.in);
 
-    while (true) {
+  public static boolean checarCPF(String cpf) {
+    for (usuario user : usuarios) {
+      if (cpf.equals(user.cpf)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean checarISBN(int isbn) {
+    for (livro exemplar : livros) {
+      if (exemplar.isbn == isbn) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int getIndexComCPF(String cpf) {
+    for (int i = 0; i < usuarios.size(); i++) {
+      if (cpf.equals(usuarios.get(i).cpf)) {
+        return i;
+    }
+    }
+    return 0;
+  }
+
+  public static int getIndexComISBN(int isbn) {
+    for (int i = 0; i < livros.size(); i++) {
+      if (isbn == livros.get(i).isbn) {
+        return i;
+    }
+    }
+    return 0;
+  }
+
+  public static void menuRegistro(){
+    boolean continuar = true;
+
+    while (continuar) {
       System.out.print("************************************************\n");
       System.out.print("*  DIGITE O NUMERO OPÇÃO DA OPERAÇÃO DESEJADA  *\n");
       System.out.print("************************************************\n");
       System.out.print("*             1 - USUARIO                      *\n");
       System.out.print("*             2 - LIVRO                        *\n");
       System.out.print("*             3 - EMPRESTIMO                   *\n");
+      System.out.print("*             4 - VOLTAR                       *\n");
       System.out.print("************************************************\n");
 
-      int opcao = subMenuRegistro.nextInt();
-
-      if (opcao == 6) {
-        System.out.println("\nPrograma encerrado\n");
-        subMenuRegistro.close();
-        break;
-      }
+      int opcao = scanner.nextInt();
 
       switch (opcao) {
         case 1:
           System.out.println("\nREGISTRAR USUARIO\n");
-          // Add code to register user
-          Scanner userScanner = new Scanner(System.in);
+
           System.out.print("Digite o nome do usuário: ");
-          String nomeUsuario = userScanner.nextLine();
+          String nomeUsuario = scanner.nextLine();
+          scanner.nextLine();
           System.out.print("Digite o cpf do usuário: ");
-          String cpfUsuario = userScanner.nextLine();
+          String cpfUsuario = scanner.nextLine();
           System.out.print("Digite o telefone do usuário: ");
-          String telefoneUsuario = userScanner.nextLine();
+          String telefoneUsuario = scanner.nextLine();
 
           usuarios.add(new usuario(nomeUsuario, cpfUsuario, telefoneUsuario));
           break;
         case 2:
           System.out.println("\nREGISTRAR LIVRO\n");
-          // Add code to register book
-          Scanner bookScanner = new Scanner(System.in);
+
           System.out.print("Digite o título do livro: ");
-          String nomeLivro = bookScanner.nextLine();
+          String nomeLivro = scanner.nextLine();
+          scanner.nextLine();
           System.out.print("Digite o autor do livro: ");
-          String autorLivro = bookScanner.nextLine();
+          String autorLivro = scanner.nextLine();
           System.out.print("Digite o editora do livro: ");
-          String editoraLivro = bookScanner.nextLine();
+          String editoraLivro = scanner.nextLine();
           System.out.print("Digite o ano do livro: ");
-          int anoLivro = bookScanner.nextInt();
+          int anoLivro = scanner.nextInt();
           System.out.print("Digite o edição do livro: ");
-          int edicaoLivro = bookScanner.nextInt();
+          int edicaoLivro = scanner.nextInt();
           System.out.print("Digite o isbn do livro: ");
-          int isbnLivro = bookScanner.nextInt();
+          int isbnLivro = scanner.nextInt();
 
           livros.add(new livro(nomeLivro, autorLivro, editoraLivro, anoLivro, edicaoLivro, isbnLivro));
           
           break;
         case 3:
           System.out.println("\nREGISTRAR EMPRESTIMO\n");
-          // Add code to register loan
-          Scanner loanScanner = new Scanner(System.in);
+          scanner.nextLine(); // evita que o scanner do cpf receba o /n deixado pelo ultimo nextLine e não receba o cpf
           System.out.print("Digite o cpf do usuário: ");
-          String cpfUsuarioReg = loanScanner.nextLine(); 
+          String cpfUsuarioReg = scanner.nextLine();
 
-          System.out.print("Digite o cpf do usuário: ");
-          String loanUser = loanScanner.nextLine();
-          System.out.print("Digite o cpf do usuário: ");
-          String loanUser = loanScanner.nextLine();
-          System.out.print("Digite o cpf do usuário: ");
-          String loanUser = loanScanner.nextLine();
-          // Save loan information
+          if (checarCPF(cpfUsuarioReg)) {
+            System.out.print("Digite o ISBN do livro: ");
+            int isbnInput = scanner.nextInt();
+            if (checarISBN(isbnInput)) {
+              System.out.print("Digite a data de emprestimo: ");
+              String dataEmprestimo = scanner.nextLine();
+              scanner.nextLine();
+              System.out.print("Digite a data de devolução: ");
+              String dataDevolucao = scanner.nextLine();
+              emprestimos.add(new emprestimo(livros.get(getIndexComISBN(isbnInput)), usuarios.get(getIndexComCPF(cpfUsuarioReg) ), dataEmprestimo, dataDevolucao));
+            }
+            else {
+              System.out.print("O livro não existe.");
+            }
+          }
+          else {
+            System.out.print("Usuário não existe.\n");
+          }
+          break;
+        case 4:
+          continuar = false;
+          break;
         default:
           System.out.println("\nOPÇÃO INVÁLIDA\n");
           break;
@@ -84,9 +131,9 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    Scanner menu = new Scanner(System.in);
+    boolean continuar = true;
 
-    while (true) {
+    while (continuar) {
       System.out.print("************************************************\n");
       System.out.print("*  DIGITE O NUMERO OPÇÃO DA OPERAÇÃO DESEJADA  *\n");
       System.out.print("************************************************\n");
@@ -97,13 +144,7 @@ public class Main {
       System.out.print("*             5 - SAIR                         *\n");
       System.out.print("************************************************\n");
 
-      int opcao = menu.nextInt();
-
-      if (opcao == 5) {
-        System.out.println("\nPrograma encerrado\n");
-        menu.close();
-        break;
-      }
+      int opcao = scanner.nextInt();
 
       switch (opcao) {
         case 1:
@@ -119,6 +160,11 @@ public class Main {
         case 4:
           System.out.println("\nDELETAR\n");
           break;
+        case 5:
+          System.out.println("\nPrograma encerrado\n");
+          scanner.close();
+          continuar = false;
+        break;
         default:
           System.out.println("\nOPÇÃO INVÁLIDA\n");
           break;
